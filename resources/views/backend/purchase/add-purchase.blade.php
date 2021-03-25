66805e24cb46f8432f3911a9c84fe1cb15281fd3
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-03-25 21:00:01 +0600

    purchase js

--- resources/views/backend/purchase/add-purchase.blade.php
+++ resources/views/backend/purchase/add-purchase.blade.php
@@ -100,7 +100,7 @@
                                         <tr>
                                             <td colspan="5"></td>
                                             <td>
-                                                <input type="text" name="estimated_amount" value="0" id="estimated_amout" class="form-control form-control-sm text-right estimated_amount" readonly style="background-color: #D8FDBA"/>
+                                                <input type="text" name="estimated_amount" value="0" id="estimated_amount" class="form-control form-control-sm text-right estimated_amount" readonly style="background-color: #D8FDBA"/>
                                             </td>
                                             <td></td>
                                         </tr>
@@ -134,7 +134,10 @@
                 <input type="hidden" name="product_id[]" value="@{{ product_id }}"> @{{ product_name }}
             </td>
             <td>
-                <input type="number" min="1" name="buying_qty[]" value="1" class="form-control form-control-sm text-right unit_price">
+                <input type="number" min="1" name="buying_qty[]" value="1" class="form-control form-control-sm text-right buying_qty">
+            </td>
+            <td>
+                <input type="number" min="1" name="unit_price[]" value="" class="form-control form-control-sm text-right unit_price">
             </td>
             <td>
                 <input type="text" name="description[]" class="form-control form-control-sm">
@@ -144,6 +147,77 @@
             </td>
             <td><a class="btn btn-danger btn-sm removeeventmore"><i class="fa fa-window-close"></i></a></td>
         </tr>
+    </script>
+{{--    add table --}}
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(document).on("click", ".addeventmore", function () {
+                var date = $('#date').val();
+                var purchase_no = $('#purchase_no').val();
+                var supplier_id = $('#supplier_id').val();
+                var category_id = $('#category_id').val();
+                var category_name = $('#category_id').find('option:selected').text();
+                var product_id = $('#product_id').val();
+                var product_name = $('#product_id').find('option:selected').text();
+
+                if(date==''){
+                    $.notify("Date is required", {globalPosition: 'top right', className: 'error'});
+                    return false;
+                }
+                if(purchase_no==''){
+                    $.notify("Parchase Number is required", {globalPosition: 'top right', className: 'error'});
+                    return false;
+                }
+                if(supplier_id==''){
+                    $.notify("Supplier is required", {globalPosition: 'top right', className: 'error'});
+                    return false;
+                }
+                if(category_id==''){
+                    $.notify("Category is required", {globalPosition: 'top right', className: 'error'});
+                    return false;
+                }
+                if(product_id==''){
+                    $.notify("Product is required", {globalPosition: 'top right', className: 'error'});
+                    return false;
+                }
+
+                var source = $("#document-template").html();
+                var template = Handlebars.compile(source);
+                var data = {
+                    data:data,
+                    purchase_no:purchase_no,
+                    supplier_id:supplier_id,
+                    category_id:category_id,
+                    category_name:category_name,
+                    product_id:product_id,
+                    product_name:product_name
+                };
+                var html = template(data);
+                $("#addRow").append(html);
+            });
+            $(document).on("click", ".removeeventmore", function (event) {
+                $(this).closest(".delete_add_more_item").remove();
+                totalAmountPrice();
+            });
+            $(document).on("keyup click", ".unit_price, .buying_qty", function () {
+                var unit_price = $(this).closest("tr").find("input.unit_price").val();
+                var qty = $(this).closest("tr").find("input.buying_qty").val();
+                var total = unit_price*qty;
+                $(this).closest("tr").find("input.buying_price").val(total);
+                totalAmountPrice();
+            });
+        //    total amount
+            function totalAmountPrice() {
+                var sum = 0;
+                $(".buying_price").each(function (){
+                    var value = $(this).val();
+                    if(!isNaN(value) && value.length != 0){
+                        sum += parseFloat(value);
+                    }
+                });
+                $('#estimated_amount').val(sum);
+            }
+        });
     </script>
     <script type="text/javascript">
         $(function (){
